70230c88c0937d0d6a65faef8b2a0be42a8dd29d
macbookpro <ilmnajot2021@gmail.com>
2025-11-01 09:34:34 +0500

    first commit

---
+++ src/main/java/uz/ilmnajot/openlibraryspringapp/OpenlibrarySpringAppApplication.java
@@ -0,0 +1,13 @@
+package uz.ilmnajot.openlibraryspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlibrarySpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenlibrarySpringAppApplication.class, args);
+    }
+
+}
